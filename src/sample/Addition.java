94f91548d35d0a0cdfc9400133d74df25cc74a84
admin <nikitaram2799@gmail.com>
2023-09-21 14:31:56 +0530

    Initial commit

---
+++ src/sample/Addition.java
@@ -0,0 +1,15 @@
+package sample;
+
+public class Addition {
+
+	
+	 static int a = 10;
+	 static int b = 20;
+	 static int c = a+b;
+			 
+		public static void main(String[] args) {
+		// TODO Auto-generated method stub
+          System.out.println(c);
+	}
+
+}
